8b1cbb843f12041a0991127b1645a6f2ac83ef00
Harry Chung <hc9825@gmail.com>
2019-11-19 08:41:41 -0800

    connection column id logic set up

--- src/friends/friends-router.js
+++ src/friends/friends-router.js
@@ -8,13 +8,30 @@ const jsonBodyParser = express.json()
 
 friendsRouter
   .route('/')
-  .get(requireAuth, async (req, res, next) => {
-    const userId = req.user.id
-    const friends = await FriendsService.getFriends(
-      req.app.get('db'),
-      userId
-    )
-    console.log(friends)
+  .get(requireAuth, (req,res,next) => {
+    FriendsService.getFriends(req.app.get('db'), req.user.id)
+      .then(async friendIds => {
+        console.log('friends', friendIds)
+        let community = []
+        let world = []
+        for (let i = 0; i < friendIds.length; i++) {
+          const acquaintances = await FriendsService.getFriends(
+            req.app.get('db'),
+            friendIds[i]
+          )
+          acquaintances.forEach(id => community.push(id))
+        }
+        console.log('community', community)
+        for (let i = 0; i < community.length; i++) {
+          const connections = await FriendsService.getFriends(
+            req.app.get('db'),
+            community[i]
+          )
+          connections.forEach(id => world.push(id))
+        }
+        console.log('world', world)
+        res.send('hi')
+      })
   })
 
 module.exports = friendsRouter
--- src/friends/friends-service.js
+++ src/friends/friends-service.js
@@ -1,20 +1,23 @@
 const xss = require('xss')
 
 const FriendsService = {
-  async getFriends(db, id) {
+  getFriends(db, id) {
     return db('users')
       .where({ id })
       .first()
       .then(user => user.friends)
   },
-  async getUserMetadata(db, id) {
-    const user = await db('users').where({ id }).first()
+  getUserMetadata(db, id) {
+    const user = db('users').where({ id }).first()
     return {
       id: user.id,
       first_name: user.first_name,
       last_name: user.last_name,
     }
-  }
+  },
+  removeDuplicateIds(ids) {
+
+  },
 }
 
 module.exports = FriendsService
