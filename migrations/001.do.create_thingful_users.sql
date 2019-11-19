b1c0982ca18d263101c6f766637074e4db8e556d
Harry Chung <hc9825@gmail.com>
2019-11-19 14:46:18 -0800

    new seeds baby

--- migrations/001.do.create_thingful_users.sql
+++ migrations/001.do.create_thingful_users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  first_name TEXT NOT NULL UNIQUE,
+  first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   password TEXT NOT NULL,
-  email TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
   date_created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   date_modified TIMESTAMP WITH TIME ZONE,
   friends integer[]
--- src/users/users-service.js
+++ src/users/users-service.js
@@ -38,9 +38,10 @@ const UsersService = {
   serializeUser(user) {
     return {
       id: user.id,
-      full_name: xss(user.full_name),
-      user_name: xss(user.user_name),
-      nickname: xss(user.nick_name),
+      first_name: xss(user.first_name),
+      last_name: xss(user.last_name),
+      email: xss(user.email),
+      password: xss(user.password),
       date_created: new Date(user.date_created),
     }
   },
