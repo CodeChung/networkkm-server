ff599953c91b753d811956efc9e855ca87b2e339
Harry Chung <hc9825@gmail.com>
2019-11-25 11:04:45 -0800

    blog lookin real good baby

--- src/blog/blog-router.js
+++ src/blog/blog-router.js
@@ -11,27 +11,36 @@ blogRouter
   .all(requireAuth)
   .get((req, res, next) => {
     BlogService.getUserBlog(req.app.get('db'), req.user.id)
-        .then(posts => res.status(201).json(posts))
+      .then(posts => res.status(201).json(posts))
   })
 
 blogRouter
   .route('/comments/:blogId')
   .all(requireAuth)
   .get((req, res, next) => {
-      const { blogId } = req.params 
-      BlogService.getBlogComments(req.app.get('db'), blogId)
-        .then(comments => res.status(201).json(comments))
+    const { blogId } = req.params
+    BlogService.getBlogComments(req.app.get('db'), blogId)
+      .then(comments => res.status(201).json(comments))
   })
   .post(jsonBodyParser, (req, res, next) => {
-      const { comment } = req.body
-      const { blogId } = req.params
-      const newComment = {
-          blog_id: blogId,
-          user_id: req.user.id,
-          comment
-      }
-      BlogService.postComment(req.app.get('db'), newComment)
-        .then(comments => res.status(301).json(comments))
+    const { comment } = req.body
+    const { blogId } = req.params
+    const newComment = {
+      blog_id: blogId,
+      user_id: req.user.id,
+      comment
+    }
+    BlogService.postComment(req.app.get('db'), newComment)
+      .then(comments => res.status(301).json(comments))
+  })
+
+blogRouter
+  .route('/user/:id')
+  .all(requireAuth)
+  .get((req, res, next) => {
+    const { id } = req.params
+    BlogService.getUserBlog(req.app.get('db'), id)
+      .then(posts => res.status(201).json(posts))
   })
 
 module.exports = blogRouter
